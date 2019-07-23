9a1a2799a2c67102c3d0a9192820e63585906ff6
jose luis <cortezluisjose@gmail.com>
2019-07-23 00:42:34 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 def countdown
   countdown = countdown_with_sleep = 10
- while countdown < 10
+ while countdown_with_sleep < 10
  puts "#{number} SECOND(S)!"
 
  puts "HAPPY NEW YEAR!"
- countdown -= 1
+ countdown_with_sleep -= 1
 end
 end
