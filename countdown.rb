8722d457a0c600023253559faa2e531c79e31c71
jose luis <cortezluisjose@gmail.com>
2019-07-23 01:14:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 def countdown
  countdown_output = 10 
 while countdown_output < 12
-puts 
+puts runtime = Benchmark.measure { countdown_with_sleep(5) }
 puts "HAPPY NEW YEAR!"
 countdown_output -= 1
  end
