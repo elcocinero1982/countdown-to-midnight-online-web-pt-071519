89631f3e2c2c40dfd5b7a7edb9124508cb8d83ad
jose luis <cortezluisjose@gmail.com>
2019-07-23 01:13:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
 def countdown
- countdown_output = 1
-while countdown_output < 10 
+ countdown_output = 10 
+while countdown_output < 12
 puts "#{countdown_output} SECOND(S)!"
 puts "HAPPY NEW YEAR!"
 countdown_output -= 1
